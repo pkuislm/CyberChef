7923907194983fd3ae5129f1de96ca264df32efe
Pkuism <1668911954@qq.com>
2024-12-14 01:56:10 +0000

    setoutput

---
+++ src/core/operations/SetOutPut.mjs
@@ -0,0 +1,45 @@
+/**
+ * @author
+ * @copyright
+ * @license Apache-2.0
+ */
+
+import Operation from "../Operation.mjs";
+
+/**
+ * SetOutput operation
+ */
+class SetOutput extends Operation {
+
+    /**
+     * SetOutput constructor
+     */
+    constructor() {
+        super();
+
+        this.name = "Set Output";
+        this.module = "Utils";
+        this.description = "Set output.";
+        this.inputType = "string";
+        this.outputType = "string";
+        this.args = [
+            {
+                "name": "Value",
+                "type": "string",
+                "value": ""
+            }
+        ];
+    }
+
+    /**
+     * @param {string} input
+     * @param {Object[]} args
+     * @returns {string}
+     */
+    run(input, args) {
+        return args[0];
+    }
+
+}
+
+export default SetOutput;
